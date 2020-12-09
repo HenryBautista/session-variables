6f4257accc7850a15ff322372722e2ff53fce99c
Henry Eloy Bautista Coaquira <henrybautistahtt@hotmail.com>
2020-12-09 10:50:24 -0400

    firts commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using session_management.Models;
+
+namespace session_management.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        const string SessionName = "_Name";
+        const string SessionAge = "_Age";
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            HttpContext.Session.SetString(SessionName, "Henry");
+            HttpContext.Session.SetInt32(SessionAge, 28);
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            ViewBag.Name = HttpContext.Session.GetString(SessionName);  
+            ViewBag.Age = HttpContext.Session.GetInt32(SessionAge);
+            ViewData["Message"] = "Asp .net Core";
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace session_management.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
